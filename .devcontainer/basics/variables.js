707bf25cbaa1926d5df68e80a57592c065e1cd79
Ravi-Ranjan-Prakash <rrp6206@gmail.com>
2024-09-10 09:40:08 +0000

    variables

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,16 @@
+const accountId=8092768778;  //cannot be changed further
+var name="Ravi";
+let email="ravi@gmail.com";
+address="bokaro"
+
+
+//accountId=123;  //will give error assignment to const varibales
+//in var in javascript there ia always a problem of scope ie if we change that variable anywhere it will get changed
+
+name="ranjan";
+email="ahah@gmai.com";
+address="kanper";
+
+console.log(accountId);
+
+console.table([accountId,name,email,address]);
