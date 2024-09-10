fffcb0d4b3fdee6373ff70f7bde9ee48bc86d803
Ravi-Ranjan-Prakash <rrp6206@gmail.com>
2024-09-10 09:39:47 +0000

    datatypes

---
+++ .devcontainer/basics/datatpes.js
@@ -0,0 +1,21 @@
+"use strict"  //to treat all js code as new version code
+
+//alert(2+2)  //will throw error as we are using node.js not browser that we use using index.html
+
+let age=23;
+let name="ravi";
+let isLoogedin=true;
+
+//datatypes are 
+// 1 number
+//   2 string ""
+//  3  boolean  =>true/false
+// 4 null  standalone value
+// 5 undefined
+// 6 symbol=>generally used in react
+
+
+console.log(typeof(isLoogedin));
+
+console.log(typeof null)    //will show as object
+console.log(typeof undefined)  //will give undefined
