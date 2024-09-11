ce922a536c101898439ca9e82e67b2b85cb676d2
Ravi-Ranjan-Prakash <rrp6206@gmail.com>
2024-09-11 07:58:32 +0000

    conversions and comparisions

---
+++ .devcontainer/basics/comparisons.js
@@ -0,0 +1,23 @@
+//normal comparison 
+
+console.log(2>3);
+
+
+
+
+
+///comparison b/w  different datatypes
+
+console.log("2"==2);         //true     //it will convert the string into number and then will compare
+console.log("3">2);          //false            //    "
+
+console.log(null>0);         //false            //it will convert null to a number ie zero and then will compare
+console.log(null==0);        //false
+console.log(null>=0);         //true
+
+//comparing b/w different datatypes is always a bad practice it should be avoided even if javascript allows it
+// typescript is thats why introduced as they have strict rule,you will not be able to compare b/w two datatypes
+//these are the limitations of javascript as sometimes they take value of null as "NaN"  but sometimes zero
+// "===" tripeequalto not only compares the value but they also check the datatypes
+
+console.log("3"===3);
---
+++ .devcontainer/basics/stringConversions.js
@@ -0,0 +1,16 @@
+
+console.log("2"+1);
+
+console.log(1+"2");
+
+
+console.log(1+2+"3");
+
+console.log(2+"4"+3);
+
+console.log("2"+1+2);
+
+//there is a concept in javascript that if the system first reads the data as string the it wil cover all the data to string.
+// as in line no 11;
+
+//but if the earlier there is a operation on numbers before a string appears the it will do the operations as in  line no 7;
