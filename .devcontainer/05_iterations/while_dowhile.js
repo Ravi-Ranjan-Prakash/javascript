756d8a0ed041562d2dc6009c37c70b7b9dad484f
Ravi-Ranjan-Prakash <rrp6206@gmail.com>
2024-09-16 16:38:02 +0000

    while dowhile

---
+++ .devcontainer/05_iterations/while_dowhile.js
@@ -0,0 +1,28 @@
+
+let score=0;
+
+while(score<10)
+{
+    console.log(`score is ${score}`)
+    score++;
+}
+
+
+//do while loop first do work then checks condition
+
+let score1=0;
+
+do {
+    console.log(`score1 is ${score1}`)
+    score1++;
+} while (score1<10);
+
+
+
+let a=11;
+
+do {
+    console.log(`a is ${a}`);
+    a++;
+    
+} while (a<10);
